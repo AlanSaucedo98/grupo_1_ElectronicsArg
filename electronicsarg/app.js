10e2039363dbb7f30ee530d51af37bc4d32be892
Lucas Vinotti <hp-lucas@hotmail.com>
2020-08-07 00:29:17 -0300

    Cree carpeta controllers, archivos views de cada url, archivo productCard en routes

--- electronicsarg/app.js
+++ electronicsarg/app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+/*var productCartRouter = require(".routes/productCart");*/
+
 var app = express();
 
 // view engine setup
