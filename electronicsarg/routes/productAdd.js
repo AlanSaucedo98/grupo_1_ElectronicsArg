5ff6f362211e9b335eb23d76286f23ea1a4db812
Franco19junio1996@gmail.com <franco19junio1996@gmail.com>
2020-08-14 12:36:22 -0300

    arreglanco detalles

--- electronicsarg/routes/productAdd.js
+++ electronicsarg/routes/productAdd.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
  /* Funcion que renderiza Al formulario de registro. */
 router.get('/', function(req, res, next) {
-  res.render('productAdd', { title: 'Formulario de Registro' });
+  res.render('productAdd', { title: 'Carga de Producto' });
 });
 
 module.exports = router;
