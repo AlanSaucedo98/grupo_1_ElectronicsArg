ef9b3beef3017d6edf382cb240ac294b6620b4f3
Lucas Vinotti <hp-lucas@hotmail.com>
2020-08-27 12:31:18 -0300

    correcciones en dcdiseño de  productCart

--- electronicsarg/routes/productCart.js
+++ electronicsarg/routes/productCart.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('productCart', { title: 'ElectronicsArg' });
+  res.render('productCart', { title: 'Carro de Compras' });
 });
 
 module.exports = router;
