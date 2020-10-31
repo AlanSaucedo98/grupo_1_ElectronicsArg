eaec7d56371cfc5b74cc0cd61b100baf284eeedc
Franco19junio1996@gmail.com <franco19junio1996@gmail.com>
2020-10-31 16:03:17 -0300

    Validacion

---
+++ electronicsarg/public/js/script-productAdd.js
@@ -0,0 +1,106 @@
+window.addEventListener("load", function () {
+
+
+    let formularioB = document.getElementById("FormuB");
+    formularioB.addEventListener("submit", function (e) {
+
+
+
+        let errores = []
+
+        let nombre = document.getElementsByClassName("nombreP");
+        if (nombre.value == "") {
+            errores.push("Nombre Obligatorio")
+
+        }
+        let precio = document.getElementsByClassName("precioP");
+        if (precio.value == "") {
+            errores.push("Especificado Precio")
+        }
+
+        let plataforma = document.getElementById("Plataforma");
+        if (plataforma.value == "") {
+            errores.push("Eliga al menos 1 plataforma")
+        }
+
+        let categoria = document.getElementById("categoria");
+        if (categoria.value == "") {
+            errores.push("Eliga al menos 1 categoria")
+        }
+
+        let idiomas = document.getElementsByClassName("idiomas");
+        if (idiomas.value == "") {
+            errores.push("Espesificar idomas")
+        }
+
+        let desarollador = document.getElementsByClassName("desarrollador");
+        if (desarollador.value == "") {
+            errores.push("Falta nombre de Desarrollador")
+        }
+
+        let procesador = document.getElementsByClassName("pro");
+        if (procesador.value == "") {
+            errores.push("Espesificar Procesador")
+        }
+
+
+        let memoria = document.getElementsByClassName("memoria");
+        if (memoria.value == "") {
+            errores.push("Espesificar memoria RAM")
+        }
+
+
+        let disco = document.getElementsByClassName("disco");
+        if (disco.value == "") {
+            errores.push("Espesificar espacio de disco duro")
+        }
+
+        let graficos = document.getElementsByClassName("graficos");
+        if (graficos.value == "") {
+            errores.push("Espesificar Graficos")
+        }
+
+        let trailer = document.getElementsByClassName("trailer");
+        if (trailer.value == "") {
+            errores.push("Trailer obligatorio")
+        }
+
+        let resumen = document.getElementsByClassName("resumen");
+        if (resumen.value == "") {
+            errores.push("Falta un Resumen")
+        }
+
+        let fecha = document.getElementsByClassName("datebirth");
+
+        if (fecha.value == "") {
+            errores.push("El campo fecha no puede estar vacio")
+
+        }
+
+        
+
+
+
+
+
+
+        if (errores.length > 0) {
+            e.preventDefault();
+
+
+
+            let ulErrores = document.querySelector("div.errores ul")
+
+            ulErrores.innerHTML = ""
+
+            for (let i = 0; i < errores.length; i++) {
+
+                ulErrores.innerHTML += "<li>" + errores[i] + "</li>"
+
+
+            }
+
+        }
+
+    })
+})
--- electronicsarg/src/routes/productEdit.js
+++ electronicsarg/src/routes/productEdit.js
@@ -1,27 +1,12 @@
 var express = require('express');
 var router = express.Router();
-var multer = require('multer');
-var path = require('path');
 
 const controller = require('../controllers/productController');
-
-
 const sessionUserCheck = require('../middlewares/sessionUserCheck');
-
-let storage = multer.diskStorage({
-    destination:(req,file,callback)=>{
-        callback(null,'public/images/products')
-    },
-    filename:(req,file,callback)=>{
-        callback(null,file.fieldname + Date.now() + path.extname(file.originalname))
-    }
-})
-
-let upload = multer({storage:storage})
-
+const multerProduct = require('../middlewares/multerProduct');
 
 router.get('/:id',sessionUserCheck , controller.editar);
-router.put('/:id',upload.any(),sessionUserCheck, controller.actualizar);
+router.put('/:id',multerProduct.any(),sessionUserCheck, controller.actualizar);
 
 
 module.exports = router;
