2bc182a4cf7d6f144cb8c468c6a52d3f8e3d4f81
Lucas Vinotti <hp-lucas@hotmail.com>
2020-11-20 01:36:58 -0300

    cambios de diseño en el home

--- electronicsarg/src/database/config/config.js
+++ electronicsarg/src/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports ={
   "development": {
     "username": "root",
-    "password": "root",
+    "password": null,
     "database": "mydb",
     "host": "127.0.0.1",
     "dialect": "mysql",
